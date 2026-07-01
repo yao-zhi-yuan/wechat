413e30272843c6ae67fe3c20d840e6f9ec4e9394
yaozhiyuan.123 <yaozhiyuan.123@bytedance.com>
2026-07-01 14:28:33 +0800

    chore: configure cloud environment

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,10 +1,11 @@
 App({
   globalData: {
-    envId: '',
+    envId: 'cloud1-d8g5p1ynefb5a2e03',
     userOpenId: ''
   },
   onLaunch() {
     wx.cloud.init({
+      env: 'cloud1-d8g5p1ynefb5a2e03',
       traceUser: true
     });
   }
